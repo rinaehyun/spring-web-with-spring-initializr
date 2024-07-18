3cc0b22cb3656d62ffcc380f64f37b10c6416157
Rinae Hyun <rnhyun.dream@gmail.com>
2024-07-18 11:56:52 +0200

    Use PathVariable to have dynamic path

--- src/main/java/com/example/spring_web_with_spring_initializr/HelloWorldController.java
+++ src/main/java/com/example/spring_web_with_spring_initializr/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.example.spring_web_with_spring_initializr;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +14,12 @@ public class HelloWorldController {
         String greeting = "Hello, World!";
         System.out.println(greeting);
         return greeting;
+    }
+
+    @GetMapping("/hello/{name}")
+    public String getGreetingWithName(@PathVariable String name) {
+        String greetingWithName = "Hello, " + name + "!";
+        System.out.println(greetingWithName);
+        return greetingWithName;
     }
 }
