38a90d5c413e339e6fd61811ffc92aaba4c53df7
Rinae Hyun <rnhyun.dream@gmail.com>
2024-07-18 11:39:24 +0200

    Initial commit

---
+++ src/main/java/com/example/spring_web_with_spring_initializr/SpringWebWithSpringInitializrApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_web_with_spring_initializr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebWithSpringInitializrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebWithSpringInitializrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_web_with_spring_initializr/SpringWebWithSpringInitializrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_web_with_spring_initializr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebWithSpringInitializrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
