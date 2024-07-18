5dcb7faa6ecbbce22568f80132dc4f14deaa1829
Rinae Hyun <rnhyun.dream@gmail.com>
2024-07-18 12:24:20 +0200

    Use POST request to get a JSON data and return it as String

--- src/main/java/com/example/spring_web_with_spring_initializr/HelloWorldController.java
+++ src/main/java/com/example/spring_web_with_spring_initializr/HelloWorldController.java
@@ -1,9 +1,13 @@
 package com.example.spring_web_with_spring_initializr;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.springframework.http.HttpEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
@@ -21,5 +25,18 @@ public class HelloWorldController {
         String greetingWithName = "Hello, " + name + "!";
         System.out.println(greetingWithName);
         return greetingWithName;
+    }
+
+    @PostMapping("/messages")
+    public String storeMessage(@RequestBody ObjectNode objectNode) {
+
+        String name = objectNode.get("name").asText();
+        String message = objectNode.get("message").asText();
+        String id = objectNode.get("id").asText();
+
+        Message message1 = new Message(name, message, id);
+        List<Message> messages = List.of(message1);
+        System.out.println(messages);
+        return messages.toString();
     }
 }
