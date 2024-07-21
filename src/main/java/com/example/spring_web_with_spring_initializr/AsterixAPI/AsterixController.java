8f994e251c44597ef9c3a1795ace3542f7dcc774
Rinae Hyun <rnhyun.dream@gmail.com>
2024-07-21 20:01:01 +0200

    Create a class Character and test GET method to retrieve List of Characters

---
+++ src/main/java/com/example/spring_web_with_spring_initializr/AsterixAPI/AsterixController.java
@@ -0,0 +1,28 @@
+package com.example.spring_web_with_spring_initializr.AsterixAPI;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/asterix")
+public class AsterixController {
+
+    @GetMapping("/characters")
+    public List<Character> getCharacters() {
+        return List.of(
+                new Character("1", "Asterix", 35, "Krieger"),
+                new Character("2", "Obelix", 35, "Lieferant"),
+                new Character("3", "Miraculix", 60, "Druide"),
+                new Character("4", "Majestix", 60, "Häuptling"),
+                new Character("5", "Troubadix", 25, "Barden"),
+                new Character("6", "Gutemine", 35, "Häuptlingsfrau"),
+                new Character("7", "Idefix", 5, "Hund"),
+                new Character("8", "Geriatrix", 70, "Rentner"),
+                new Character("9", "Automatix", 35, "Schmied"),
+                new Character("10", "Grockelix", 35, "Fischer")
+        );
+    }
+}
---
+++ src/main/java/com/example/spring_web_with_spring_initializr/AsterixAPI/Character.java
@@ -0,0 +1,9 @@
+package com.example.spring_web_with_spring_initializr.AsterixAPI;
+
+public record Character(
+        String id,
+        String name,
+        int age,
+        String profession
+) {
+}
